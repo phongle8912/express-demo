8325b292e9b0cc97a4288d013c7af76324ad5df6
peter.le <peter.le@beyondedge.com.sg>
2019-11-27 15:52:43 +0700

    add pug engine

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route('/').get((req, res) => {
+  res.sendFile(process.cwd() + '/views/pub/index.pug');
+});
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log('Listening on port ' + process.env.PORT);
 });
